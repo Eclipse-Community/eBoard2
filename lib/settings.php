739b181c0dae1fafd48011791431bd3df0b50fe6
R3n <r3n@gmx.com>
2021-11-08 20:48:40 -0600

    Placeholder settings.php

---
+++ lib/settings.php
@@ -0,0 +1,42 @@
+<?php
+//Generated and parsed by the Board Settings admin panel.
+
+//Settings
+$boardname = 'Board²';
+$logoalt = 'Board²';
+$logotitle = 'Board²';
+$dateformat = 'm/d/Y, H:i:s';
+$autoLockMonths = 0;
+$warnMonths = 0;
+$customTitleThreshold = 0;
+$viewcountInterval = 10000;
+$overallTidy = NULL;
+$noAjax = false;
+$noGuestLayouts = false;
+$theWord = '';
+$systemUser = 0;
+$minWords = 0;
+$minSeconds = 15;
+$uploaderCap = 60.0;
+$personalCap = 0.25;
+$uploaderMaxFileSize = 10.0;
+$uploaderWhitelist = 'png gif jpg jpeg txt css zip rar 7z ogg mp3 swf ogv mp4 webm';
+$mailResetFrom = 'webmaster@eclipse.cx';
+$lastPostsTimeLimit = 72;
+
+//Hacks
+$hacks['forcetheme'] = '';
+$hacks['themenames'] = 0;
+
+//Profile Preview Post
+$profilePreviewText = '[url=https://www.youtube.com/watch?v=YNSh86RTxog&t=3s]No fuck off[/url]';
+
+//Meta
+$metaDescription = '';
+$metaKeywords = '';
+
+//RSS
+$feedname = 'Board² RSS';
+$rssblurb = 'The latest stuffs on the board.';
+
+?>

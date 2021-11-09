f0722aad009d6800fbd4ab72c6e0ed8ad34a9533
R3n <r3n@gmx.com>
2021-11-08 21:02:11 -0600

    Uploaded wrong settings.php, fuck

--- lib/settings.php
+++ lib/settings.php
@@ -2,17 +2,17 @@
 //Generated and parsed by the Board Settings admin panel.
 
 //Settings
-$boardname = 'Board²';
-$logoalt = 'Board²';
-$logotitle = 'Board²';
+$boardname = 'eBoard²';
+$logoalt = 'Another eBoard² Board';
+$logotitle = 'Another eBoard² Board';
 $dateformat = 'm/d/Y, H:i:s';
 $autoLockMonths = 0;
 $warnMonths = 0;
 $customTitleThreshold = 0;
 $viewcountInterval = 10000;
-$overallTidy = NULL;
-$noAjax = false;
-$noGuestLayouts = false;
+$overallTidy = 0;
+$noAjax = 0;
+$noGuestLayouts = 1;
 $theWord = '';
 $systemUser = 0;
 $minWords = 0;
@@ -21,7 +21,7 @@ $uploaderCap = 60.0;
 $personalCap = 0.25;
 $uploaderMaxFileSize = 10.0;
 $uploaderWhitelist = 'png gif jpg jpeg txt css zip rar 7z ogg mp3 swf ogv mp4 webm';
-$mailResetFrom = 'webmaster@eclipse.cx';
+$mailResetFrom = 'noreply@example.net';
 $lastPostsTimeLimit = 72;
 
 //Hacks
@@ -29,14 +29,17 @@ $hacks['forcetheme'] = '';
 $hacks['themenames'] = 0;
 
 //Profile Preview Post
-$profilePreviewText = '[url=https://www.youtube.com/watch?v=YNSh86RTxog&t=3s]No fuck off[/url]';
+$profilePreviewText = 'This is a sample post. It demonstrates various kinds of [i]markup[/i] and quotes and such.
+This has to be one of the [b]longest[/b] preview messages ever seen in an eBoard².[quote=Barack Obama]I am Barack Obama and I approve this preview message.[quote=some guy]This is a nested quote. See what you get.[/quote][/quote]Well, what more could you [url=http://en.wikipedia.org]want to know[/url]? Perhaps how to do the classic infinite loop?
+[source=qbasic]10 PRINT "Hello World!"
+20 GOTO 10[/source]HEEEEY MACARONI! :D';
 
 //Meta
-$metaDescription = '';
-$metaKeywords = '';
+$metaDescription = 'The owner of this eBoard² board was too lazy to edit the description field. Kill him.';
+$metaKeywords = 'eboard²,board';
 
 //RSS
-$feedname = 'Board² RSS';
-$rssblurb = 'The latest stuffs on the board.';
+$feedname = 'New eBoard² Feed';
+$rssblurb = 'The latest replies on the board.';
 
 ?>

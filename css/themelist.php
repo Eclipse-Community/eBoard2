aceba60f03089ad5cd442f92225919228eaf3d2b
K4sum1 <k4sum1@eclipse.cx>
2021-11-13 06:32:34 -0600

    Squared theme and much improved dark

--- css/themelist.php
+++ css/themelist.php
@@ -9,6 +9,7 @@ $themes = array(
 	"default"=>"eBoard2 (Default)",
 
 	//Packins
+	"defaultsquared"=>"eBoard2 Squared",
 	"eboard2dark"=>"eBoard2 Dark",
 	"abxd20"=>"ABXD 2.0",
 	"defaultold"=>"ABXD Classic (rounded)",
@@ -24,9 +25,10 @@ $themes = array(
 );
 
 $themeBylines = array(
-	"default"=>"eBoard2 theme by K4sum1",
+	"default"=>"eBoard2 default theme<br />By K4sum1",
     
-    "eboard2dark"=>"eBoard2 theme, but dark(er)<br />Also by K4sum1",
+	"defaultsquared"=>"eBoard2 theme, but without rounded edges<br />By K4sum1",
+    "eboard2dark"=>"eBoard2 theme, but dark(er)<br />By K4sum1",
 	"abxd20"=>"Design by Kawa",
 	"cheese"=>"Original by Acmlm<br />Ported by Kawa",
 	"cycle"=>"Design by Kawa",
